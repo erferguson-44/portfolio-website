eab201036d964bda52f94fdf5d7435b2baeae20a
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-08-14 15:37:56 -0700

    Skills -- updated and adjusted width of imgs

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
+			xlarge:   [ '1281px',  '2000px' ],
 			large:    [ '981px',   '1280px' ],
 			medium:   [ '737px',   '980px'  ],
 			small:    [ '481px',   '736px'  ],
